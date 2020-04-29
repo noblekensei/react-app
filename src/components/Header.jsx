fcb16c35b84f63822dc0a5c41b27d371cf734293
noblekensei <honourable.kensei@gmail.com>
2020-04-29 10:11:09 +0800

    v1

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header className="header">
+			<img src="https://dcassetcdn.com/design_img/1559024/551167/551167_7840631_1559024_911ff84c_image.png" />
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className="content">
+			<div>
+				<img src="https://wallup.net/wp-content/uploads/2017/03/28/436394-ultra-wide-photography-landscape-nature-748x316.png" />M/
+			</div>
+			<div>
+				<img src="https://cdn-images-1.medium.com/max/1200/1*ktnvPoq-yPznt7WkezOkBg.png" />
+				Description
+			</div>
+			<div>
+				My Posts
+				<div>New Post</div>
+			</div>
+			<div>
+				<div>Post 1</div>
+				<div>Post 2</div>
+				<div>Post 3</div>
+			</div>
+		</div>
+	);
+};
+
+export default Profile;
